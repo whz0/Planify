e275c227b0495abca29c4ab83fe7816bca7b6c24
adrizz8 <adriro11@ucm.es>
2025-03-09 18:39:59 +0100

    Entity de evento

--- planifyAPI/src/main/java/com/planifyAPI/planifyAPI/entity/Evento.java
+++ planifyAPI/src/main/java/com/planifyAPI/planifyAPI/entity/Evento.java
@@ -1,6 +1,7 @@
 package com.planifyAPI.planifyAPI.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,4 +9,11 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class Evento {
+    private String nombre;
+    private String fecha;
+    private String hora;
+    private String ubicacion;
+    @Id
+    private Long id_evento;
+
 }
