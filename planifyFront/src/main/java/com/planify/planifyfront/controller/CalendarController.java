0117a23cd11536472131934196064b69b0787826
guillesanper <guilsa06@ucm.es>
2025-03-09 18:13:45 +0100

    Controllers arreglados

--- planifyFront/src/main/java/com/planify/planifyfront/controller/CalendarController.java
+++ planifyFront/src/main/java/com/planify/planifyfront/controller/CalendarController.java
@@ -16,11 +16,6 @@ public class CalendarController {
     public void initialize() {
         // Crear el calendario para el mes actual o el que desees mostrar
         FullCalendarView calendarView = new FullCalendarView(YearMonth.now());
-        // Suponiendo que fullCalendarView es una instancia de FullCalendarView
-        LocalDate eventDate = LocalDate.of(2025, 3, 15); // fecha del evento
-
-
-
         // Agregar la vista del calendario al Pane definido en el FXML
         calendarPane.getChildren().add(calendarView.getView());
     }
--- planifyFront/src/main/java/com/planify/planifyfront/controller/DashboardController.java
+++ planifyFront/src/main/java/com/planify/planifyfront/controller/DashboardController.java
@@ -26,41 +26,6 @@ public class DashboardController {
         calendarPane.getChildren().add(fullCalendarView.getView());
     }
 
-    // Método para manejar el botón "Inicio"
-    @FXML
-    public void handleInicio() {
-        mostrarAlerta("Inicio", "Navegando a la sección de Inicio.");
-        // Aquí puedes añadir lógica para cargar contenido relacionado con "Inicio".
-    }
-
-    // Método para manejar el botón "Calendario"
-    @FXML
-    public void handleCalendario() {
-        mostrarAlerta("Calendario", "Ya estás en el calendario.");
-        // Aquí puedes añadir lógica para cargar contenido relacionado con "Calendario".
-    }
-
-    // Método para manejar el botón "Tareas"
-    @FXML
-    public void handleTareas() {
-        mostrarAlerta("Tareas", "Navegando a la sección de Tareas.");
-        // Aquí puedes añadir lógica para cargar contenido relacionado con "Tareas".
-    }
-
-    // Método para manejar el botón "Configuración"
-    @FXML
-    public void handleConfiguracion() {
-        mostrarAlerta("Configuración", "Navegando a la Configuración.");
-        // Aquí puedes añadir lógica para cargar contenido relacionado con "Configuración".
-    }
-
-    // Método para manejar el botón "Cerrar Sesión"
-    @FXML
-    public void handleCerrarSesion() {
-        mostrarAlerta("Cerrar Sesión", "Has cerrado sesión.");
-        // Aquí puedes agregar lógica para cerrar la sesión del usuario.
-    }
-
     // Método auxiliar para mostrar alertas de información
     private void mostrarAlerta(String titulo, String contenido) {
         Alert alerta = new Alert(Alert.AlertType.INFORMATION);
