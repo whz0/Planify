faa0e8e153ba913bc9d2223f304c98725a4d64fb
guillesanper <guilsa06@ucm.es>
2025-03-09 17:32:45 +0100

    Commit inicial vacio

--- planifyAPI/src/main/java/com/planifyAPI/planifyAPI/controller/EventController.java
+++
@@ -1,9 +0,0 @@
-package com.planifyAPI.planifyAPI.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class EventController {
-
-}
--- planifyAPI/src/main/java/com/planifyAPI/planifyAPI/service/EventoService.java
+++
@@ -1,4 +0,0 @@
-package com.planifyAPI.planifyAPI.service;
-
-public class EventoService {
-}
