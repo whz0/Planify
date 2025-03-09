eb0c7cb09351de32ff691708d14cc77d60ae047c
whz0 <wzhu01@ucm.es>
2025-03-09 18:58:20 +0100

    spring library in SAEvento

--- planifyAPI/src/main/java/com/planifyAPI/planifyAPI/service/SAEvento.java
+++ planifyAPI/src/main/java/com/planifyAPI/planifyAPI/service/SAEvento.java
@@ -5,6 +5,7 @@ import com.planifyAPI.planifyAPI.entity.Evento;
 import com.planifyAPI.planifyAPI.repository.EventoRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
