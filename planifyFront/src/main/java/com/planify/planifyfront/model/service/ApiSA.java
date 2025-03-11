d8a483a0263dd56d80f70033f6f9b6184ad19344
guillesanper <guilsa06@ucm.es>
2025-03-11 20:23:42 +0100

    Api service funcionando

--- planifyFront/src/main/java/com/planify/planifyfront/model/service/ApiSA.java
+++ planifyFront/src/main/java/com/planify/planifyfront/model/service/ApiSA.java
@@ -1,8 +1,10 @@
 package com.planify.planifyfront.model.service;
 
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.planify.planifyfront.model.transfer.TEvento;
 import javafx.concurrent.Task;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -19,21 +21,33 @@ public class ApiSA {
     public ApiSA() {
         this.httpClient = HttpClient.newHttpClient();
         this.objectMapper = new ObjectMapper();
+        this.objectMapper.registerModule(new JavaTimeModule());
     }
 
     public Task<String> createEvent(TEvento evento) {
         return new Task<>() {
             @Override
             protected String call() throws Exception {
-                String requestBody = objectMapper.writeValueAsString(evento);
-                HttpRequest request = HttpRequest.newBuilder()
-                        .uri(new URI(BASE_URL + "/create-event"))
-                        .header("Content-Type", "application/json")
-                        .POST(HttpRequest.BodyPublishers.ofString(requestBody))
-                        .build();
+                try {
+                    String requestBody = objectMapper.writeValueAsString(evento);
+                    HttpRequest request = HttpRequest.newBuilder()
+                            .uri(new URI(BASE_URL + "/create-event"))
+                            .header("Content-Type", "application/json")
+                            .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                            .build();
 
-                HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-                return response.body();
+                    HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+                    if (response.statusCode() != 200) {
+                        throw new IOException("HTTP error code: " + response.statusCode());
+                    }
+
+                    return response.body();
+                } catch (Exception e) {
+                    System.err.println("Error during API call: " + e.getMessage());
+                    e.printStackTrace();
+                    throw e;
+                }
             }
         };
     }
