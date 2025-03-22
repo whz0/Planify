83cae82a81730295e568dc973dd10890eb7e7095
whz0 <wzhu01@ucm.es>
2025-03-22 19:50:40 +0100

    entity calendario

---
+++ planifyAPI/src/main/java/com/chilltime/planifyapi/entity/Calendario.java
@@ -0,0 +1,21 @@
+package com.chilltime.planifyapi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Calendario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private boolean activo;
+
+}
