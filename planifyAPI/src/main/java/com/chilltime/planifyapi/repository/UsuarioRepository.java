fda91218219c082bd1ff6eafcc7ae529c7e55d42
guillesanper <guilsa06@ucm.es>
2025-03-22 19:45:10 +0100

    Repository Usuario hecho

---
+++ planifyAPI/src/main/java/com/chilltime/planifyapi/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package com.chilltime.planifyapi.repository;
+
+import com.chilltime.planifyapi.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario,Integer> {
+}
