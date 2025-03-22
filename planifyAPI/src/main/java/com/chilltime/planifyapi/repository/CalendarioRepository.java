cc7f96792d6aa375d762cfc0e24914bf0f77ad37
adrizz8 <adriro11@ucm.es>
2025-03-22 19:49:13 +0100

    SACalendario

---
+++ planifyAPI/src/main/java/com/chilltime/planifyapi/repository/CalendarioRepository.java
@@ -0,0 +1,8 @@
+package com.chilltime.planifyapi.repository;
+
+import com.chilltime.planifyapi.entity.Calendario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CalendarioRepository extends JpaRepository<Calendario, Integer> {
+
+}
