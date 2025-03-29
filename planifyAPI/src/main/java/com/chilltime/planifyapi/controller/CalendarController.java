53dfc8b2287a06bd7e69146a116f3c12ad74374e
whz0 <wzhu01@ucm.es>
2025-03-29 09:18:44 +0100

    Pruebas Integración Calendar Controller

--- planifyAPI/src/main/java/com/chilltime/planifyapi/controller/CalendarController.java
+++ planifyAPI/src/main/java/com/chilltime/planifyapi/controller/CalendarController.java
@@ -22,7 +22,7 @@ public class CalendarController {
             cont = calendarService.createPrivateCalendar(cal);
         }
         catch (Exception e) {
-            cont = new TContext(200, e.getMessage(), null);
+            cont = new TContext(400, e.getMessage(), null);
         }
 
         return ResponseEntity.status(cont.getStatus_code()).body(cont);
