9375056e11b1dc4f0e0e876e9ebf4e21b93f934a
TanggWoo <scrist01@ucm.es>
2025-03-29 14:05:20 +0100

    Creada y agregada vista de generar código de invitación

---
+++ planifyFront/src/main/java/com/chilltime/planifyfront/controller/GenerateCodeFormController.java
@@ -0,0 +1,43 @@
+package com.chilltime.planifyfront.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class GenerateCodeFormController {
+
+    @FXML
+    private TextField codeTextField;
+
+    @FXML
+    private void initialize() {
+        generateNewCode();
+    }
+
+    private void generateNewCode() {
+        String code = generateRandomCode(8);
+        codeTextField.setText(code);
+    }
+
+    private String generateRandomCode(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder code = new StringBuilder();
+
+        for(int i = 0; i < length; i++) {
+            int index = (int) (Math.random() * characters.length());
+            code.append(characters.charAt(index));
+        }
+        return code.toString();
+    }
+
+    @FXML
+    private void handleGenerate() {
+        generateNewCode();
+    }
+
+    @FXML
+    private void handleCancel() {
+        Stage stage = (Stage) codeTextField.getScene().getWindow();
+        stage.close();
+    }
+}
