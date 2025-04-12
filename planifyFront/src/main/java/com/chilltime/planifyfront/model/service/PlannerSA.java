27c1cb9427486001287f8e6ba808892c14f555af
adrizz8 <adriro11@ucm.es>
2025-04-12 17:44:24 +0200

    Creación del PlannerSA y transfer TPlanner necesario para la entidad en el frontend

---
+++ planifyFront/src/main/java/com/chilltime/planifyfront/model/service/PlannerSA.java
@@ -0,0 +1,33 @@
+package com.chilltime.planifyfront.model.service;
+
+import com.chilltime.planifyfront.model.transfer.TPlanner;
+import javafx.concurrent.Task;
+
+import java.io.IOException;
+
+public class PlannerSA {
+    private ApiClient apiClient;
+
+    private final String BASE_URL = "/planner";
+
+    public PlannerSA(ApiClient apiClient) {
+        this.apiClient = apiClient;
+    }
+
+    public Task<String> registerPlanner(TPlanner planner) {
+      return new Task<>() {
+          @Override
+          protected String call() throws Exception {
+              try {
+                  String requestBody = apiClient.getObjectMapper().writeValueAsString(planner);
+                  return apiClient.post(BASE_URL + "/register",requestBody);
+              }
+
+              catch (IOException e) {
+                  System.err.println("Error during API call: " + e.getMessage());
+                  throw e;
+              }
+          }
+      };
+    }
+}
---
+++ planifyFront/src/main/java/com/chilltime/planifyfront/model/transfer/TPlanner.java
@@ -0,0 +1,65 @@
+package com.chilltime.planifyfront.model.transfer;
+
+public class TPlanner {
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
+    private boolean active;
+
+    public TPlanner(Long id, String username, String password, String role, boolean active) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.active = active;
+    }
+
+    public TPlanner() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean getActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return "Planner: " + username + " " + password;
+    }
+
+}
