33c9207070f6fc79b18a09c232185cc287b7792d
guillesanper <guilsa06@ucm.es>
2025-04-13 22:48:37 +0200

    Imagen aplicada a login.fxml

--- planifyFront/src/main/java/com/chilltime/planifyfront/controller/LoginController.java
+++ planifyFront/src/main/java/com/chilltime/planifyfront/controller/LoginController.java
@@ -2,17 +2,14 @@ package com.chilltime.planifyfront.controller;
 
 import com.chilltime.planifyfront.App;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 
-public class LoginController implements Initializable {
+public class LoginController {
     @FXML
     private Label lblErrors;
 
@@ -28,18 +25,13 @@ public class LoginController implements Initializable {
     @FXML
     private Button btnSignin;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        // Puedes inicializar algunos elementos aquí si es necesario
-    }
-
     @FXML
     public void handleButtonAction(MouseEvent event) {
         if (event.getSource() == btnSignup) {
             App.changeView("register","Register");
         }
         if (event.getSource() == btnSignin) {
-            App.changeView("dashboard","Dashboard");
+            //TODO: Implementar la lógica de inicio de sesión
         }
     }
 }
