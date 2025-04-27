fc89d44a6479433ee5e95a83ef48a93b28769428
adrizz8 <adriro11@ucm.es>
2025-04-27 21:09:20 +0200

    Test del frontend arreglados y cambios en el controller de planner para ello

--- planifyFront/src/main/java/com/chilltime/planifyfront/controller/RegisterFormController.java
+++ planifyFront/src/main/java/com/chilltime/planifyfront/controller/RegisterFormController.java
@@ -45,18 +45,15 @@ public class RegisterFormController {
         try {
 
             if(username.getText().isEmpty() || password.getText().isEmpty()) {
-                showErrorDialog("Error de validación", "Por favor, completa todos los campos.");
-                return;
+                lblErrors.setText("El usuario ha dejado alguno de los campos vacíos");
             }
 
             if(username.getText().length() > 15) {
-                showErrorDialog("Error de validación", "El nombre de usuario no es válido. Debe tener máximo 15 caracteres.");
-                return;
+                lblErrors.setText("El nombre de usuario no es válido. Debe tener máximo 15 caracteres");
             }
 
             if(password.getText().length() < 8 || password.getText().length() > 15) {
-                showErrorDialog("Error de validación", "La contraseña no es válida. Debe tener entre 8 y 15 caracteres.");
-                return;
+                lblErrors.setText("La contraseña no es válida. Debe tener entre 8 y 15 caracteres");
             }
 
             TPlanner planner = new TPlanner();
